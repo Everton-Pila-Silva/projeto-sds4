39612471b3ce7987b8b757c7a15df0140e5e0876
Everton Pila Silva <84790670+Everton-Pila-Silva@users.noreply.github.com>
2021-09-09 23:27:36 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/everton-pila-silva" target="_blank" rel="noreferrer">Everton Pila Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/everton_p_s" target="_blank" rel="noreferrer">@evertonpila</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
